fc8031ba145168f685353e09026555e1c2638006
Devin Benninghoven <devinbenninghoven@gmail.com>
2023-02-18 18:26:58 -0800

    updated main.py to parse data

--- src/main.py
+++ src/main.py
@@ -1,26 +1,5 @@
 import json
 
-data = {
-    "bob" : "course"
-        }
-
-with open("courses.json", "w") as write_file:
-    json.dump(data, write_file)
-
-file = open("../data/manualentery.txt")
-
-def GetCode(string):
-    if len(string) != 3 and len(string) != 4:
-        return ""
-    count = 0
-    for x in string:
-        count += 1
-        if count > 3:
-            break
-        if not x.isdigit():
-            return ""
-    return string
-
 def GetName(string):
     counter = 0
     for char in string:
@@ -31,22 +10,80 @@ def GetName(string):
         return ""
     return string
 
-# deptcode coursenum courseletter coursename credits
-# course description
+DEPTCODE = ""
+COURSENUM = ""
+COURSELETTER = ""
+COURSENAME = ""	
+COURSEDESCRIPTION = ""
+CREDITS = ""
+GRADCREDIT = ""
+MAJMINREQ = ""		
+
+courses_dict = {}
+
+count = 0
+
+PATH = "../data/manualentery.txt"
+file = open(PATH, "r")
 
 for line in file.readlines():
-    isName = False
-    lines = line.split()
-    name = GetName(lines[0])
-    code = GetCode(lines[1])
-    desc = ""
-    if name and code:
-        isName = True
-    if isName:
-        desc = line.split("-")[1]
-        desc = desc.split("(")[0]
-        desc = desc[1:]
-        print(f"{name} - {code} - {desc}")
+    count += 1
+    words = line.split()
+    if GetName(words[0]):
+        # RESET VARS
+        DEPTCODE = ""
+        COURSENUM = ""
+        COURSELETTER = ""
+        COURSENAME = ""	
+        CREDITS = ""
+        count = 0
+        # DEPTCODE
+        DEPTCODE = words[0]
+        temp = words[1][-1]
+        # COURSENUM & COURSELETTER
+        if temp.isalpha():
+            COURSELETTER = temp
+            COURSENUM = words[1][:-1]
+        else:
+            COURSELETTER = "N/A"
+            COURSENUM = words[1]
+        # COURSENAME 
+        listOfNames = words[3:-1]
+        for name in listOfNames:
+            COURSENAME += name
+            COURSENAME += " " 
+        COURSENAME = COURSENAME[:-1]
+        # CREDITS
+        CREDITS = words[-1]
+        CREDITS = CREDITS[1:-1]
+        if len(CREDITS) > 1:
+            x = CREDITS[0]
+            y = CREDITS[-1]
+            CREDITS = x if int(x) > int(y) else y
+    # COURSEDESCRIPTION
+    elif count == 1:
+        COURSEDESCRIPTION = line
+        COURSEDESCRIPTION = COURSEDESCRIPTION[:-1]
+
+    # GRADCREDIT
+    else:
+        if "400-level" in line:
+            GRADCREDIT = "True"
+        else:
+            GRADCREDIT = "False"
+        if "quisite" in line:
+            listOfClasses = line.split(":")[1]
+            print(listOfClasses)
+
+        
+
+courses_dict[f"{DEPTCODE}{COURSENUM}{COURSELETTER}"] = {}
+with open("../data/courses.json", "w") as outfile:
+    json.dump(courses_dict, outfile)
+
+# TODO - write to the json file every 4 or 3 lines
+
+
 
 
 print("ran")
